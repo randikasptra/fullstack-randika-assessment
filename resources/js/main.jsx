e912c10502867a1ed6add91b171f221029ff92c7
randikasptra <saputrarandika857@gmail.com>
2025-10-11 04:52:25 +0700

    feat(middleware) : Add all the required middleware

--- resources/js/main.jsx
+++ resources/js/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app'
 
+
 // render react di elemen id="app" yang ada di welcome.blade.php
 ReactDOM.createRoot(document.getElementById('app')).render(
     <React.StrictMode>
