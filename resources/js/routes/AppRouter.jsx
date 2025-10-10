efe47ddd2ddeadb991c1bf6fa490bfc8eef31d23
randikasptra <saputrarandika857@gmail.com>
2025-10-11 04:04:38 +0700

    fix(Auth) :Auth error nothing else middleware

---
+++ resources/js/routes/AppRouter.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Import pages
+import LoginPage from "../auth/LoginPage";
+import RegisterPage from "../auth/RegisterPage";
+import DashboardAdmin from "../pages/admin/DashboardAdmin";
+import DashboardUser from "../pages/user/DashboardUser";
+
+
+ReactDOM.createRoot(document.getElementById("app")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/admin/dashboard" element={<DashboardAdmin />} />
+        <Route path="/user/dashboard" element={<DashboardUser />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+export default AppRouter;
