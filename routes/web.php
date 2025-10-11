3d5f06d07ec72d55e663cc2ec2f3dda352e146da
randikasptra <saputrarandika857@gmail.com>
2025-10-11 19:59:17 +0700

    feat(middleware): add middleware for authLogin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Admin\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
