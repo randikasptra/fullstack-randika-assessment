acc2defa3901bee6b00deb586918d7002e0e3715
randikasptra <saputrarandika857@gmail.com>
2025-10-12 07:14:23 +0700

    fix(Kernel): revisi in kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,6 +14,10 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        'auth' => \App\Http\Middleware\Authenticate::class,
+    'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+    'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+    'login' => \App\Http\Middleware\EnsureUserIsLoggedIn::class,
 
     ];
 
