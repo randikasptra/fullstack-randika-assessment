c3d4a90f98babb2fb18ca727d2cc10a643a96ebd
randikasptra <saputrarandika857@gmail.com>
2025-10-12 18:12:52 +0700

    fix(books): upload image succes

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,8 +60,16 @@ return [
             'report' => false,
         ],
         'cloudinary' => [
-            'driver' => 'cloudinary',
+        'driver' => 'cloudinary',
+        'cloud' => [
+            'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+            'api_key'    => env('CLOUDINARY_API_KEY'),
+            'api_secret' => env('CLOUDINARY_API_SECRET'),
         ],
+        'url' => [
+            'secure' => true,
+        ],
+    ],
 
 
     ],
