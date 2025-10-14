5a730557a46df950cf5430e37b54b45b0c24d37a
randikasptra <saputrarandika857@gmail.com>
2025-10-14 16:18:31 +0700

    feat(websocket: error configuration

--- resources/js/main.jsx
+++ resources/js/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
+import './bootstrap';
 
 // render react in elemen id="app" in welcome.blade.php
 ReactDOM.createRoot(document.getElementById("app")).render(
